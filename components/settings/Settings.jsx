c0bcc896de7098355a7364e6f3e59ba48d7d8d0c
Mikel Duffy <mikelduffy1@gmail.com>
2017-01-09 11:39:30 -0800

    Adds transfers

--- components/settings/Settings.jsx
+++ components/settings/Settings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './Nav.jsx';
-import Addresses from './Addresses.jsx'
+import Addresses from './Addresses.jsx';
+import Transfers from './Transfers.jsx';
 
 class Settings extends React.Component {
   constructor(props) {
@@ -17,6 +18,7 @@ class Settings extends React.Component {
         <Nav toRoute={this.changeRoute.bind(this)}/>
       Settings
         <Addresses deep={this.props.deep} userID={this.props.userData.userID} />
+        <Transfers deep={this.props.deep} userID={this.props.userData.userID} />
       </div>
       )
   }
