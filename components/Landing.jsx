7c67ba613863507f73a380448f4d6d79246e93c8
Matthew Goldwater <mattgwater@gmail.com>
2017-01-09 13:23:31 -0800

    Change data from defaulting to userId Kai to data from login function

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -11,14 +11,35 @@ let options = {
 class Landing extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.updateInput = this.updateInput.bind(this);
   }
+
+  updateInput(key) {
+    let state = {}
+    return function(e) {
+      state[key] = e.target.value;
+      this.setState(
+        state
+      )
+    }.bind(this)
+  } 
 
   submitHandler(e) {
     e.preventDefault();
     //sign in
-    this.props.deep.login({}, (success, data) => {
+    this.props.deep.login({
+      role: 'user',
+      username: this.state.username,
+      password: this.state.password
+    }, (success, data) => {
+      console.log('success is: ', success, 'data is: ', data);
       if (success) {
-        data = {userID: 'harry'};
+        // data = {userID: 'kai'};
+        data = data;
         this.props.getUserData(data);
         this.props.router.push('/dashboard');
       } else {
@@ -42,11 +63,13 @@ class Landing extends React.Component {
         <div className='loginForm'>
           <form onSubmit={(e) => this.submitHandler(e)}>
             <label>
-              <input type="text" placeholder="username" name="name" />
+              <input type="text" onChange={this.updateInput('username')} placeholder="username" name="name" />
               <br /><br />
-              <input type="text" placeholder="password" name="name" />
+              <input type="text" onChange={this.updateInput('password')} placeholder="password" name="name" />
             </label>
             <br /><br />
+            {console.log(this.state.username)}
+            {console.log(this.state.password)}
             <input type="submit" value="Log In" />&nbsp;&nbsp;
             <button onClick={(e) => this.clickHandler(e)}> Sign Up </button>
           </form>
