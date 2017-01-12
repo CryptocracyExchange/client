501773b5ebee6c7c95f141a31fac32ca398cd2cb
Mikel Duffy <mikelduffy1@gmail.com>
2017-01-11 19:32:35 -0800

    Fixes subscription bug

--- components/App.jsx
+++ components/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // const url = process.env.NODE_ENV === 'prod' ? 'deepstream' : 'localhost';
 // const url = '192.241.227.176'; // Need to change to production IP/URL when deploying
 const url = 'localhost'; // Need to change to production IP/URL when deploying
-const client = require('deepstream.io-client-js')(`${url}:6020`);
+const client = window.deepstream(`${url}:6020`);
 
 class App extends React.Component {
   constructor(props) {
