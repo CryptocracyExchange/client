ea7784ede95930b6935c0ed1ae9da94c51d4de90
Mikel Duffy <mikelduffy1@gmail.com>
2017-01-15 22:00:20 -0800

    Adds production webpack config

--- server/server.js
+++ server/server.js
@@ -3,29 +3,32 @@ const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const chalk = require('chalk');
-const webpack = require('webpack');
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
-const config = require('../webpack.config.js');
 
-const compiler = webpack(config);
 const app = express();
 
-// app.use(morgan('combined'));
-app.use(webpackDevMiddleware(compiler, {
-  hot: true,
-  filename: 'bundle.js',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true
-}));
- 
-app.use(webpackHotMiddleware(compiler, {
-  log: console.log,
-  path: '/__webpack_hmr',
-  heartbeat: 10 * 1000,
-}));
+if (process.env.NODE_ENV !== 'prod') {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
+  const config = require('../webpack.config.js');
+  const compiler = webpack(config);
+
+  app.use(webpackDevMiddleware(compiler, {
+    hot: true,
+    filename: 'bundle.js',
+    stats: {
+      chunks:false,
+      colors: true,
+    },
+    historyApiFallback: true
+  }));
+  
+  app.use(webpackHotMiddleware(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000,
+  }));
+}
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
 var path = require('path');
-
 var webpack = require('webpack');
 
-module.exports = {
+const config = {
   entry: [
-   'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-   'webpack/hot/only-dev-server',
    './components/index.jsx'
   ],
   output: {
@@ -16,7 +13,7 @@ module.exports = {
     loaders: [
       {
         test: /.jsx?$/,
-        loaders: ['react-hot', 'babel-loader?{"presets":["es2015","react"]}'],
+        loaders: ['babel-loader?{"presets":["es2015","react"]}'],
         exclude: /node_modules/
       }
     ]
@@ -31,19 +28,33 @@ module.exports = {
       path.join(__dirname, 'node_modules'),
     ],
   },
-  devtool: '#inline-source-map',
-  plugins: [
-    // Webpack HMR for faster dev time
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-    // new webpack.DefinePlugin({  // <-- Key to reducing React's size
-    //   'process.env': {
-    //     'NODE_ENV': JSON.stringify('production')
-    //   }
-    // }),
-    // new webpack.optimize.DedupePlugin(),            // Dedupe similar code 
-    // new webpack.optimize.UglifyJsPlugin(),          // Minify everything
-    // new webpack.optimize.AggressiveMergingPlugin()  // Merge chunks 
-  ]
+  devtool: '#inline-source-map'
 };
+
+if (process.env.NODE_ENV === 'prod') {
+  config.plugins = [
+    new webpack.DefinePlugin({  // <-- Key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),            // Dedupe similar code 
+    new webpack.optimize.UglifyJsPlugin(),          // Minify everything
+    new webpack.optimize.AggressiveMergingPlugin()  // Merge chunks
+  ]
+}
+
+if (process.env.NODE_ENV !== 'prod') {
+  config.entry.unshift(
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+    'webpack/hot/only-dev-server'
+    );
+  config.module.loaders[0].loaders.unshift('react-hot');
+  config.plugins =[
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin({quiet: true}),
+    new webpack.NoErrorsPlugin()
+  ];
+}
+
+module.exports = config;
