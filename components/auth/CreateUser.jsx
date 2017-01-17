bcfaac5c89d369dc53e6a7ba63b3ec727f17c27e
Matthew Goldwater <mattgwater@gmail.com>
2017-01-16 17:54:46 -0800

    Handle errors for duplicate username and email upon signup

--- components/auth/CreateUser.jsx
+++ components/auth/CreateUser.jsx
@@ -8,20 +8,26 @@ class CreateUser extends React.Component {
       username: '',
       password: '',
       confirm: '',
-      email: ''
+      email: '',
+      error: false
     }
     this.updateFormInput = this.updateFormInput.bind(this);
+    this.updateErrorState = this.updateErrorState.bind(this);
     this.signUp = this.signUp.bind(this);
   }
-  submitHandler(e) {
-    e.preventDefault();
-    this.props.router.push('/');
-  }
 
+  submitHandler(e, success) {
+    console.log('gets into submitHandler')
+    e.preventDefault();
+    if (success) {
+      this.props.router.push('/');
+    }
+  }
+/*
   clickHandler(e) {
     e.preventDefault();
     this.props.router.push('/');
-  }
+  }*/
 
 /*<button onClick={(e) => this.clickHandler(e)}> Cancel </button>*/
 
@@ -29,11 +35,18 @@ class CreateUser extends React.Component {
     var state = {};
     console.log('this is: ', this)
     return function(e) {
+      this.updateErrorState();
       state[key] = e.target.value
       this.setState(
         state,
       )
     }.bind(this)
+  }
+
+  updateErrorState(){
+    this.setState({
+      error: false
+    })
   }
 
   signUp(e) {
@@ -43,7 +56,9 @@ class CreateUser extends React.Component {
     var email = this.state.email
     $.ajax({
       url: 'http://localhost:8999/signup',
+      // url: 'http://localhost:3001/signup',
       method: 'POST',
+      crossDomain: true,
       data: {
         username: username,
         password: password,
@@ -51,13 +66,20 @@ class CreateUser extends React.Component {
       },
       success: (message) => {
         console.log('success message is:', message);
+        this.setState({
+          error: false
+        })
+        this.submitHandler(e, true)
       },
       error: (message) => {
         console.log('error message is:', message);
+        this.setState({
+          error: message.responseText
+        })
+        this.submitHandler(e, false)
       }
     })
   }
-
 
   render() {
     let passwordDoesNotMatchOrEmpty = this.state.password.length === 0 || this.state.password !== this.state.confirm
@@ -71,17 +93,20 @@ class CreateUser extends React.Component {
           <form onSubmit={(e) => this.submitHandler(e)}>
             <label>
               <input type="text" onChange={this.updateFormInput('username')} placeholder="username" name="name" />
+              {this.state.error === 'username is already taken' && <p>username is already taken</p>}
               <br /><br />
               {console.log(this.state.username)}
               {console.log(this.state.password)}
               {console.log(this.state.confirm)}
               {console.log(this.state.email)}
+              {console.log('this.state.error is: ', this.state.error)}
               <input type="text" onChange={this.updateFormInput('password')} placeholder="password" name="name" />
               <br /><br />
               <input type="text" onChange={this.updateFormInput('confirm')} placeholder="confirm password" name="name" />
               <br /><br />
               {passwordDoesNotMatch && <p>Passwords do not match</p>}
               <input type="text" onChange={this.updateFormInput('email')} placeholder="email" name="name" />
+              {this.state.error === 'email is already taken' && <p>email is already taken</p>}
             </label>
             <br /><br />
             <input type="submit" onClick={this.signUp} value="Submit" disabled={passwordDoesNotMatchOrEmpty} />&nbsp;&nbsp;
